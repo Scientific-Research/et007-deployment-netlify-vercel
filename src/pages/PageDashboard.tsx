720f1ab007966e69402937c00ee39f9f8e0dfb8a
Maximilian <scientificresearch309@gmail.com>
2023-06-23 15:33:45 +0200

    a new photo added but in Public folder!

--- src/pages/PageDashboard.tsx
+++ src/pages/PageDashboard.tsx
@@ -4,6 +4,7 @@ export const PageDashboard = () => {
 		<div className="pageDashboard">
 			<p>This is the Dashboard page.</p>
 			<img src={employmentImage} alt="employment Sign " />
+			<img src="images/Jobs.jpeg" alt="Jobs photo" />
 		</div>
 	);
 };
